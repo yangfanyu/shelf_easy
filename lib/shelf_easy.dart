aadee8c443bf04e29cf24b596ee0ed708fb34f5d
yangfanyu <yangfanyu@aliyun.com>
2022-01-09 02:21:45 +0800

    0.0.8

--- lib/shelf_easy.dart
+++ lib/shelf_easy.dart
@@ -3,6 +3,7 @@
 ///
 library shelf_easy;
 
+export 'src/db/db_base.dart';
 export 'src/easy_class.dart';
 export 'src/easy_client.dart' show EasyClient;
 export 'src/easy_server.dart' show EasyServer;
